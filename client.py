f47541678df41af174b5c47717fcc2b85c7e9756
ahmed Waheed <aw0reborn@gmail.com>
2019-12-15 01:03:31 +0500

    somewhat

--- client.py
+++ client.py
@@ -1,10 +1,33 @@
 from socket import *
-serverName = 'localhost'
-serverPort = 7004
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-message = input("client:: enter your message ")
-clientSocket.send(message.encode())
-modifiedMessage = clientSocket.recv(1024)
-print('from server: ', modifiedMessage.decode())
-clientSocket.close()
+import json
+class client:
+    serverName = '127.0.0.1'
+    serverPort = 8951
+    def __init__(self):
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((self.serverName,self.serverPort))
+        message = "1"
+        clientSocket.send(message.encode())
+        while True: 
+            info = clientSocket.recv(1024)
+            self.display_info(self.binary_to_dict(info))
+
+    def display_info(self,info):
+        print(info)
+    def dict_to_binary(self,the_dict):
+        str = json.dumps(the_dict)
+        binary = ' '.join(format(ord(letter), 'b') for letter in str)
+        return binary
+
+
+    def binary_to_dict(self,the_binary):
+        jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
+        d = json.loads(jsn)  
+        return d
+
+    # message = input("client:: enter your message ")
+    # clientSocket.send(message.encode())
+    # modifiedMessage = clientSocket.recv(1024)
+    # print('from server: ', modifiedMessage.decode())
+    # .clientSocket.close()
+c = client()
--- server.py
+++ server.py
@@ -1,29 +1,50 @@
 from socket import *
 import threading
-
+import random
+import json
 class server:
-    serverPort = 7004
     
+    serverPort = random.randint(7000,9000)
+    print(serverPort)
+    peer = {}
     def __init__(self):
         serverSocket = socket(AF_INET, SOCK_STREAM)
         serverSocket.bind(('',self.serverPort))
         serverSocket.listen(10)
         while True:
             client_conn, addr = serverSocket.accept()
+            print(addr)
             client_conn.settimeout(60)
             threading.Thread(target = self.listenToclient,args = (client_conn,addr)).start()
     def add_peers(self,client_conn,addr):
-        peer = {}
+        self.peer[client_conn] = addr
+
 
 
 
     def listenToclient(self,client_conn,addr):
         self.add_peers(client_conn,addr)
         fileName = client_conn.recv(1024).decode()
-        print("message recv from client_conn: ", fileName)
-        fileName = fileName.upper()
-        client_conn.send(fileName.encode())
-        client_conn.close()
+        if(fileName == "1"):
+            while True:
+                print(self.peer.values())
+                client_conn.send(self.dict_to_binary(self.peer.values()))      
+        # print("message recv from client_conn: ", fileName)
+        # fileName = fileName.upper()
+        # client_conn.send(fileName.encode())
+        # print(self.peer)
+        # self.client_conn.close()
+
+    def dict_to_binary(self,the_dict):
+        str = json.dumps(the_dict)
+        binary = ' '.join(format(ord(letter), 'b') for letter in str)
+        return binary
+
+
+    def binary_to_dict(self,the_binary):
+        jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
+        d = json.loads(jsn)  
+        return d
 
     
     
