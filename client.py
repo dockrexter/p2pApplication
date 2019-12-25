47b655ec6e716a537c50b40a1dd3c24ed3ba51e5
ahmed Waheed <aw0reborn@gmail.com>
2019-12-25 20:05:46 +0500

    updated

--- client.py
+++ client.py
@@ -1,33 +1,122 @@
 from socket import *
 import json
+import _pickle as pickle
+import threading
+import sys
+
 class client:
-    serverName = '127.0.0.1'
-    serverPort = 8951
-    def __init__(self):
-        clientSocket = socket(AF_INET, SOCK_STREAM)
-        clientSocket.connect((self.serverName,self.serverPort))
-        message = "1"
-        clientSocket.send(message.encode())
-        while True: 
-            info = clientSocket.recv(1024)
-            self.display_info(self.binary_to_dict(info))
+    #defining protocols 
+    peer_port_info = "peer_port_info"
+    get_peers_info = "get_peers_info"
+    file_upload = "file_upload"
+    
+    def __init__(self,p2p_server_addr,p2p_server_port,our_port,name):
+        self.server_addr = p2p_server_addr
+        self.server_Port = p2p_server_port
+        self.peer_port = our_port
+        self.name = name
+        #to connect with server
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((self.server_addr,self.server_Port))
+        data = pickle.dumps([self.peer_port_info,self.name,self.peer_port])
+        self.socket.sendall(data)
+        hello = self.socket.recv(5012)
+        print(hello.decode())
+        # self.get_peer_info()
+        # self.get_peer_info()
+        # print(self.clientSocket)
+        #to connect with this peer
+        self.clientSocket = socket(AF_INET, SOCK_STREAM)
+        self.clientSocket.bind(('',self.peer_port))
+
+        t2 = threading.Thread(target = self.menu)
+        t2.start()
+        print("------------------------------------------")
+        # print(self.clientSocket.getsockname())
+        print("------------------------------------------")
+        self.clientSocket.listen(4)
+        print("i am here help me")
+        client_conn, addr = self.clientSocket.accept()
+        t1 = threading.Thread(target = self.listenToclient,args = (client_conn,addr))
+        t1.start()
+            # t1.join()
+
+
+    def listenToclient(self,client_conn,addr):
+        # self.add_peers(client_conn,addr)
+
+        fileName = client_conn.recv(1024).decode()
+        print(fileName)
+        print("enter q to quit")
+        if(fileName != "q"):
+            msg = input("enter your msg ")
+            client_conn.send(msg.encode())
+            self.listenToclient(client_conn,addr)
+            # client_conn.send(self.dict_to_binary(self.peer))    
+
 
     def display_info(self,info):
         print(info)
-    def dict_to_binary(self,the_dict):
-        str = json.dumps(the_dict)
-        binary = ' '.join(format(ord(letter), 'b') for letter in str)
-        return binary
+    
+    def get_peer_info(self):
+        data = pickle.dumps([self.get_peers_info])
+        self.socket.sendall(data)
+        print("i am here")
+        peer_info = self.socket.recv(5555012)
+        if peer_info == b'':
+            print("emptyy")
+            peer_info = self.socket.recv(5555012)
+            print(peer_info)
+        else:
+            print(pickle.loads(peer_info))
 
+    def connect(self):
+        self.new_Sock = socket(AF_INET, SOCK_STREAM)
+        port = int(input("enter port no"))
+        self.new_Sock.connect((self.server_addr,port))
+        self.talk()
 
-    def binary_to_dict(self,the_binary):
-        jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
-        d = json.loads(jsn)  
-        return d
+    def talk(self):
+        self.new_Sock.send(("hello from"+self.name).encode())
+        while True:
+            print("i am in the while")
+            fileName = self.new_Sock.recv(1024).decode()
+            print(fileName)
+            print("enter q to quit")
+            if(fileName != "q"):
+                msg = input("enter your msg ")
+                self.new_Sock.send(msg.encode())
+            else:
+                break
+
+    def upload_file(self):
+        fname = input("enter file name : ")
+        data = pickle.dumps([self.upload_file,fname,self.peer_port])
+        self.socket.sendall(data)
+        hello = self.socket.recv(5012)
+        print(hello.decode())
+        
+    def menu(self):
+        while True:
+            
+            print("Hello World")
+            user_input = input("enter 'show peers' to see the peers list \n enter 'connect' to connect to peer \n enter 'upload file' to upload file  ")
+            if user_input == "show peers":
+                self.get_peer_info()
+            if user_input == "connect":
+                self.connect()
+            if user_input == "upload file":
+                self.upload_file()
 
     # message = input("client:: enter your message ")
     # clientSocket.send(message.encode())
     # modifiedMessage = clientSocket.recv(1024)
     # print('from server: ', modifiedMessage.decode())
     # .clientSocket.close()
-c = client()
+name = sys.argv[1]
+our_port = int(sys.argv[2])
+p2p_server_addr = sys.argv[3]
+p2p_server_port = int(sys.argv[4])
+c = client(p2p_server_addr,p2p_server_port,our_port,name)
+
+
--- server.py
+++ server.py
@@ -2,49 +2,67 @@ from socket import *
 import threading
 import random
 import json
+import pickle
+
+
 class server:
-    
+    counter = 0
     serverPort = random.randint(7000,9000)
     print(serverPort)
-    peer = {}
+    
     def __init__(self):
-        serverSocket = socket(AF_INET, SOCK_STREAM)
-        serverSocket.bind(('',self.serverPort))
-        serverSocket.listen(10)
+        self.peer_list = []
+        self.serverSocket = socket(AF_INET, SOCK_STREAM)
+        self.serverSocket.bind(('',self.serverPort))
+        self.serverSocket.listen(10)
+        
+        self.listen()
+
+    def listen(self):
         while True:
-            client_conn, addr = serverSocket.accept()
-            print(addr)
-            client_conn.settimeout(60)
-            threading.Thread(target = self.listenToclient,args = (client_conn,addr)).start()
-    def add_peers(self,client_conn,addr):
-        self.peer[client_conn] = addr
+            
+            self.client_conn, self.addr = self.serverSocket.accept()
+            
+            threading.Thread(target = self.listenToclient).start()
+            print(self.addr)
+            # self.listenToclient()
+    # def add_peers(self,client_conn,addr):
+    #     self.peer.append(addr)
 
 
 
 
-    def listenToclient(self,client_conn,addr):
-        self.add_peers(client_conn,addr)
-        fileName = client_conn.recv(1024).decode()
-        if(fileName == "1"):
-            while True:
-                print(self.peer.values())
-                client_conn.send(self.dict_to_binary(self.peer.values()))      
+    def listenToclient(self):
+        # self.add_peers(client_conn,addr)
+        while True:
+            print("hello")
+            data = pickle.loads(self.client_conn.recv(4096))
+            print(data)
+            if(data[0] == "peer_port_info"):
+                self.peer_list.append(data)
+                self.client_conn.sendall("done".encode())
+            if(data[0] == "get_peers_info"):
+                print("line 39")
+                data = pickle.dumps(self.peer_list)
+                self.counter+=1
+                print(self.counter)
+                s = self.client_conn.sendall(data)
+                print("status",s)
+
         # print("message recv from client_conn: ", fileName)
         # fileName = fileName.upper()
         # client_conn.send(fileName.encode())
         # print(self.peer)
         # self.client_conn.close()
 
-    def dict_to_binary(self,the_dict):
-        str = json.dumps(the_dict)
-        binary = ' '.join(format(ord(letter), 'b') for letter in str)
-        return binary
+    # def dict_to_binary(self,the_dict):
+    #     data_string = pickle.dumps(the_dict) 
+    #     return data_string
 
 
     def binary_to_dict(self,the_binary):
-        jsn = ''.join(chr(int(x, 2)) for x in the_binary.split())
-        d = json.loads(jsn)  
-        return d
+        data_loaded = pickle.loads(the_binary) #data loaded.
+        return data_loaded
 
     
     
