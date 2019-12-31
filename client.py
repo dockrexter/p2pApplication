255cc745adfd116b919c524a986cd7597db12a18
ahmed Waheed <aw0reborn@gmail.com>
2019-12-31 10:01:57 +0500

    done

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
- from socket import *
+from socket import *
 import json
 import _pickle as pickle
 import threading
 import sys
+import os
 
 class client:
     #defining protocols 
     peer_port_info = "peer_port_info"
     get_peers_info = "get_peers_info"
     file_upload = "file_upload"
+    get_file_upload = "get_file_upload"
     
     def __init__(self,p2p_server_addr,p2p_server_port,our_port,name):
         self.server_addr = p2p_server_addr
@@ -45,15 +47,33 @@ class client:
     def listenToclient(self,client_conn,addr):
         # self.add_peers(client_conn,addr)
 
-        fileName = client_conn.recv(1024).decode()
-        print(fileName)
+        msg = client_conn.recv(1024).decode()
+        print(msg)
         print("enter q to quit")
-        if(fileName != "q"):
+        if msg == "talk":
             msg = input("\nenter your msg ")
             client_conn.send(msg.encode())
-            self.listenToclient(client_conn,addr)
-            # client_conn.send(self.dict_to_binary(self.peer))    
-
+            while True:
+                print("in the while line 57 ")
+                msg = client_conn.recv(1024).decode()
+                print(msg)            
+                msg = input("\nenter your msg ")
+                client_conn.sendall(msg.encode())
+                    
+                    # client_conn.send(self.dict_to_binary(self.peer))    
+        if msg == "download":
+            msg = "uploading file"
+            client_conn.send(msg.encode())
+            file_name = client_conn.recv(1024).decode()    
+            # file_path = os.path.join(os.getcwd(), '.')  # Organizing the path of file that will be Download
+            path="/home/ahmed/Desktop/desktop2/cn-lab/socket-programming/p2pApplication/SharingFiles/Uploads"
+            # file_path = os.path.join(file_path, 'SharingFiles')
+            # file_path = os.path.join(file_path, "uploads")
+            with open(os.path.join(path, file_name),  # writing to file
+                  'r') as myfile:
+                data = myfile.read()
+                print(data)
+                client_conn.send(data.encode())
 
     def display_info(self,info):
         print(info)
@@ -70,14 +90,30 @@ class client:
         else:
             print(pickle.loads(peer_info))
 
+    def get_files(self):
+        data = pickle.dumps([self.get_file_upload])
+        self.socket.sendall(data)
+        print("i am here")
+        list_info = self.socket.recv(5555012)
+        if list_info == b'':
+            print("emptyy")
+            list_info = self.socket.recv(5555012)
+            print(list_info)
+        else:
+            print(pickle.loads(list_info))
+
     def connect(self):
         self.new_Sock = socket(AF_INET, SOCK_STREAM)
         port = int(input("enter port no"))
         self.new_Sock.connect((self.server_addr,port))
-        self.talk()
+        usr_input = input("\n enter 'talk' to starting chating \n enter 'download' to download file")
+        if usr_input == "talk":
+            self.talk()
+        if usr_input == "download":
+            self.download_file()
 
     def talk(self):
-        self.new_Sock.send(("hello from"+self.name).encode())
+        self.new_Sock.send(("talk").encode())
         while True:
             print("waiting for reply \n")
             fileName = self.new_Sock.recv(1024).decode()
@@ -91,22 +127,46 @@ class client:
 
     def upload_file(self):
         fname = input("enter file name : ")
-        data = pickle.dumps([self.upload_file,fname,self.peer_port])
+        data = pickle.dumps([self.file_upload,fname,self.peer_port])
         self.socket.sendall(data)
         hello = self.socket.recv(5012)
         print(hello.decode())
+    
+    def download_file(self):
+        self.new_Sock.send(("download").encode())
+        file_name = input("enter file name to download ")
+        msgg = self.new_Sock.recv(1024).decode()
+        print("uploading ",msgg)
+        self.new_Sock.send(file_name.encode())
+        file_path = os.path.join(os.getcwd(), '.')  # Organizing the path of file that will be Download
+        file_path = os.path.join(file_path, 'SharingFiles')
+        file_path = os.path.join(file_path, "downloads")
+
+        with open(os.path.join(file_path, file_name),  # writing to file
+                  'wb') as myfile:
+            while True:
+                data = self.new_Sock.recv(1024)
+                print(data)
+                if not data:
+                    myfile.close()
+                    break
+                myfile.write(data)
+        print('File Downloaded Successfully')
+
         
     def menu(self):
         while True:
             
             print(" \n Hello World")
-            user_input = input("enter 'show peers' to see the peers list \n enter 'connect' to connect to peer \n enter 'upload file' to upload file  \n")
+            user_input = input("enter 'show peers' to see the peers list \n enter 'connect' to connect to peer \n enter 'upload file' to upload file  \n enter 'get files' to get available files")
             if user_input == "show peers":
                 self.get_peer_info()
             if user_input == "connect":
                 self.connect()
             if user_input == "upload file":
                 self.upload_file()
+            if user_input =="get files":
+                self.get_files()
 
     # message = input("client:: enter your message ")
     # clientSocket.send(message.encode())
